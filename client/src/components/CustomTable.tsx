82a5e8f66f25f03e844ec463c607cfe023cd020e
Daniel <daniel001.dhf@gmail.com>
2024-11-12 22:34:34 +0800

    Added Row Selection To Table For Mobile

--- client/src/components/CustomTable.tsx
+++ client/src/components/CustomTable.tsx
@@ -6,7 +6,7 @@ import { Veterinarian } from "../models/Veterinarian";
 import { Case } from "../models/Case";
 import { PetOwner } from "../models/PetOwner";
 import { Pet } from "../models/Pet";
-import { FaArrowUp, FaArrowDown, FaPlus, FaRegMinusSquare, FaEdit, FaFilter } from "react-icons/fa";
+import { FaArrowUp, FaArrowDown, FaPlus, FaRegMinusSquare, FaEdit, FaFilter, FaThList } from "react-icons/fa";
 import { RiArrowLeftWideFill, RiArrowRightWideFill } from "react-icons/ri";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import { Paginator } from "./Paginator";
@@ -67,18 +67,19 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
 
   const headers = React.useMemo(() => columns.map(x => x.header), [columns])
 
-  const [currentMobileHeader, setCurrentMobileHeader] = React.useState<typeof headers[number]>(headers[0]);
+  const [currentMobileHeader, setCurrentMobileHeader] = React.useState<typeof headers[number]>(headers[1]);
+  const [isMobileSelectModeOn, setIsMobileSelectModeOn] = React.useState(false);
 
   const goToNextMobileHeader = () => {
     const currentIndex = headers.indexOf(currentMobileHeader);
     if (currentIndex === headers.length - 1) {
-      return setCurrentMobileHeader(headers[0]);
+      return setCurrentMobileHeader(headers[1]);
     }
     return setCurrentMobileHeader(headers[currentIndex + 1]);
   }
   const goToPreviousMobileHeader = () => {
     const currentIndex = headers.indexOf(currentMobileHeader);
-    if (currentIndex === 0) {
+    if (currentIndex === 1) {
       return setCurrentMobileHeader(headers[headers.length - 1]);
     }
     return setCurrentMobileHeader(headers[currentIndex - 1]);
@@ -88,29 +89,36 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
     ref: addButtonRef,
     showTooltip: showAddButtonTooltip,
     hideTooltip: hideAddButtonTooltip,
-    TooltipElement: addButtonTooltipElement
+    TooltipElement: AddButtonTooltipElement
   } = useTooltip<HTMLButtonElement>('Add row');
 
   const {
     ref: deleteButtonRef,
     showTooltip: showDeleteButtonTooltip,
     hideTooltip: hideDeleteButtonTooltip,
-    TooltipElement: deleteButtonTooltipElement
+    TooltipElement: DeleteButtonTooltipElement
   } = useTooltip<HTMLButtonElement>('Delete row');
 
   const {
     ref: editButtonRef,
     showTooltip: showEditButtonTooltip,
     hideTooltip: hideEditButtonTooltip,
-    TooltipElement: editButtonTooltipElement
+    TooltipElement: EditButtonTooltipElement
   } = useTooltip<HTMLButtonElement>('Edit row');
 
   const {
     ref: filterButtonRef,
     showTooltip: showFilterButtonTooltip,
     hideTooltip: hideFilterButtonTooltip,
-    TooltipElement: filterButtonTooltipElement
+    TooltipElement: FilterButtonTooltipElement
   } = useTooltip<HTMLButtonElement>('Filter columns');
+
+  const {
+    ref: selectButtonRef,
+    showTooltip: showSelectButtonTooltip,
+    hideTooltip: hideSelectButtonTooltip,
+    TooltipElement: SelectButtonTooltipElement
+  } = useTooltip<HTMLButtonElement>('Select');
   return(<>
   <div
     onClick={toggleDropdown}
@@ -132,7 +140,7 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
             className="rounded-md bg-white shadow-md px-4 py-2 hover:bg-slate-100">
             <FaPlus />
           </button>
-          {addButtonTooltipElement}
+          {AddButtonTooltipElement}
 
           <button
             ref={deleteButtonRef}
@@ -141,7 +149,7 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
             className="rounded-md bg-white shadow-md px-4 py-2 hover:bg-slate-100">
             <FaRegMinusSquare />
           </button>
-          {deleteButtonTooltipElement}
+          {DeleteButtonTooltipElement}
 
           <button
             ref={editButtonRef}
@@ -150,7 +158,17 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
             className="rounded-md bg-white shadow-md px-4 py-2 hover:bg-slate-100">
             <FaEdit />
           </button>
-          {editButtonTooltipElement}
+          {EditButtonTooltipElement}
+
+          <button
+            onClick={() => setIsMobileSelectModeOn(x => !x)}
+            ref={selectButtonRef}
+            onMouseEnter={showSelectButtonTooltip}
+            onMouseLeave={hideSelectButtonTooltip}
+            className={`rounded-md shadow-md px-4 py-2 hover:bg-slate-100 ${isMobileSelectModeOn ? 'bg-slate-200' : 'bg-white'}`}>
+            <FaThList />
+          </button>
+          {SelectButtonTooltipElement}
         </div>
         <div
           className="hidden sm:inline-block">
@@ -165,7 +183,7 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
             <Dropdown 
               columns={table.getAllColumns()}/>}
           </button>
-          {filterButtonTooltipElement}
+          {FilterButtonTooltipElement}
         </div>
         </header>
       {
@@ -176,7 +194,7 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
         <thead 
           className="sticky top-[-1rem]">
             {table.getHeaderGroups().map(headerGroup => (
-              <tr
+              <tr 
                 key={headerGroup.id}>
                 {headerGroup.headers.map(header => ( 
                   header.id !== 'select' ?
@@ -221,7 +239,9 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
           <tbody 
             className="bg-inherit">
             {table.getFilteredRowModel().rows.length > 0 ? table.getRowModel().rows.map(row => (
-              <tr 
+              <tr
+                onClick={() => row.toggleSelected()}
+                className={row.getIsSelected() ? 'bg-slate-200' : ''}
                 key={row.id}>
                 {row.getVisibleCells().map(cell => (
                   cell.column.id !== 'select' ?
@@ -288,10 +308,21 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
                 {headerGroup.headers.map(header => (
                   header.column.columnDef.header === currentMobileHeader ?
                   <th
-                    className="px-3 py-2 cursor-pointer select-none first:rounded-tl-xl last:rounded-tr-xl"
-                    onClick={() => header.column.toggleSorting()}
+                    className="flex px-3 py-2 cursor-pointer select-none first:rounded-tl-xl last:rounded-tr-xl"
                     key={header.id}>
+                        { isMobileSelectModeOn &&
+                          table
+                            .getAllLeafColumns()
+                            .indexOf(header.column)
+                          === 1 ?
+                          flexRender(
+                            table.getFlatHeaders()[0].column.columnDef.header,
+                            header.getContext()
+                          ) :
+                          undefined
+                        }
                       <div
+                        onClick={() => header.column.toggleSorting()}
                         className="flex gap-4 justify-center items-center">
                         <div 
                           >
@@ -327,9 +358,19 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
                   <td 
                     key={cell.id}>
                       <div
-                        onClick={() => row.toggleExpanded()}
                         className="flex justify-between gap-4 p-2 hover:bg-slate-300">
                         <div>
+                        { isMobileSelectModeOn &&
+                          table
+                            .getAllLeafColumns()
+                            .indexOf(cell.column)
+                          === 1 ?
+                          flexRender(
+                            table.getAllLeafColumns()[0].columnDef.cell,
+                            cell.getContext()
+                          ) :
+                          undefined
+                        }
                           {
                             flexRender(
                               cell.column.columnDef.cell,
@@ -337,7 +378,8 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
                             )
                           }
                         </div>
-                        <button>
+                        <button
+                          onClick={() => row.toggleExpanded()}>
                           {
                             row.getIsExpanded() ?
                             <IoMdArrowDropup /> :
@@ -358,6 +400,7 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
                               className="grid grid-cols-2 gap-x-8 gap-y-4 text-xs">
                                 {
                                   row.getVisibleCells().map(x => (
+                                    x.column.columnDef.id !== 'select' ?
                                     <div
                                       className="flex flex-col gap-1">
                                       <div
@@ -368,13 +411,13 @@ T extends User | Admin | PetOwner | Pet | Veterinarian | Case | Medicine
                                       <div>
                                         {x.renderValue() as ReactNode}
                                       </div>
-                                    </div>
+                                    </div> : null
                                   ))
                                 }
-                            </div>
+                            </div>                            
                         </td>
                       }
-                  </td> : undefined
+                  </td> : null
                   ))}
               </tr>
             )) : 
