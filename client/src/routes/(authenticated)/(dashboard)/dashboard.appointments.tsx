4c71d61ea2170d7d3b88191c82ef723836843d06
Daniel <daniel001.dhf@gmail.com>
2024-11-14 23:50:43 +0800

    Added react-scheduler Dependency For Appointment Component

--- client/src/routes/(authenticated)/(dashboard)/dashboard.appointments.tsx
+++ client/src/routes/(authenticated)/(dashboard)/dashboard.appointments.tsx
@@ -1,5 +1,30 @@
+import { Scheduler } from '@aldabil/react-scheduler'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(authenticated)/(dashboard)/dashboard/appointments')({
-  component: () => <div>Hello /(aawdwd)</div>
+  component: Appointments
 })
+
+function Appointments() {
+  return(
+    <div>
+<Scheduler
+  view="month"
+  events={[
+    {
+      event_id: 1,
+      title: "Event 1",
+      start: new Date("2021/5/2 09:30"),
+      end: new Date("2021/5/2 10:30"),
+    },
+    {
+      event_id: 2,
+      title: "Event 2",
+      start: new Date("2021/5/4 10:00"),
+      end: new Date("2021/5/4 11:00"),
+    },
+  ]}
+/>
+    </div>
+  )
+}
